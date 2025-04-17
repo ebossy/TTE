90d3f387123b71106240bc09062c6fb42c3d271f
ebossy <enesyilmaz26@hotmail.de>
2025-04-18 00:05:19 +0200

    Filterung von todos

--- src/app/features/todo/pages/todo/todo.component.ts
+++ src/app/features/todo/pages/todo/todo.component.ts
@@ -20,30 +20,70 @@ import {Observable, of} from 'rxjs';
     NgForOf,
     MatButton,
     MatIcon,
-    AsyncPipe
   ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
+
+/**
+ * Page zeigt todos an
+ */
 export class TodoComponent implements OnInit{
   todos$: Observable<Todo[]> = of([]);
+
+  //Array welches angezeit wird
+  filteredTodos: Todo[] = [];
+
+  filter: 'all' | 'done' | 'open' = 'all';
+
+
   async ngOnInit() {
     this.todos$ = await this.todoFire.getUserTodos()
+
+    //Bei jeder änderung Filter aufrufen
+    this.todos$.subscribe(todos => {
+      this.applyFilter(todos);
+    });
   }
 
 
   test() {
     console.log(this.todoFire.getUserTodos());
   }
+
   constructor(
     private dialog: MatDialog,
     private todoFire: TodoFirestoreService) {}
 
+
   openTodoDialog() {
-    const dialogRef = this.dialog.open(TodoFormComponent, {
+    this.dialog.open(TodoFormComponent, {
       width: '40%',
       disableClose: true
     });
   }
 
+  //Filtert aus dem Observable was angezeigt werden soll
+  applyFilter(todos: Todo[]) {
+    switch (this.filter) {
+      case 'done':
+        this.filteredTodos = todos.filter(t => t.status);
+        break;
+      case 'open':
+        this.filteredTodos = todos.filter(t => !t.status);
+        break;
+      case 'all':
+      default:
+        this.filteredTodos = todos;
+    }
+  }
+
+  /**
+   * funktion wird von buttons aus der html aufgerufen
+   * @param value Filter wert
+   */
+  setFilter(value: 'all' | 'done' | 'open') {
+    this.filter = value;
+    this.todos$.subscribe(todos => this.applyFilter(todos)); // Filter neu anwenden
+  }
 }
